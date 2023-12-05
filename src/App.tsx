bf71743a7813a8f829819099fa838f709126c3e5
Charles <charles.chrismann@free.fr>
2023-12-05 11:30:06 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-red-500">
+      Hello world!
+    </h1>
+    <h2>
+      *ff
+      <span>yes</span>
+    </h2>
+    </>
+  )
+}
+
+export default App
